fc45bc2a8e9d122cc6b687d9f2ae35dce3d17cd6
sofia tofigh <sst2140@columbia.edu>
2020-03-06 06:01:05 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,14 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board=["O", "X", " ", " ", "X", " ", "X", "O", " "]
+
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+
+display_board(board)
